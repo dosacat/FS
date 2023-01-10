726cae4e7dd74c6c9b4a115544b5a4fcd13fa259
Princess,best person ever <tiwari.amanda@gmail.com>
2023-01-10 17:30:21 +0530

    Excercises 3.1-3.6

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-console.log("Yo, this works!")
+const phonebook = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get("/api/persons",(req,res)=>{
+  res.json(phonebook)
+})
+
+app.post("/api/persons",(req,res)=>{
+  res.status(200).end()
+})
+
+
+app.get("/info",(req,res)=>{
+  const total=phonebook.length
+  const time = Date()
+  res.json(`This phonebook has info of ${total} people ${time}`)
+})
+
+
+app.get("/api/persons/:id",(req,res)=>{
+  var pid = req.params.id
+  const entry = phonebook.find(elem=>elem.id == pid)
+  if (entry) {
+    res.json(entry)
+  } 
+  else{
+    res.status(404).end()  
+  }
+  
+})
+
+app.delete("/api/persons/:id",(req,res)=>{
+  const id = Number(req.params.id)
+  phonesbook= phonebook.filter(elem=>elem.id===id)
+  response.status(204).end()
+  
+})
+
+
+const port = 3001
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`)
+})
+
